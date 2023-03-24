44b52cfe5d0b1ceaed681beba2449f5fc40c44f7
Danimal <danimal@modestprogress.com>
2023-03-24 15:24:37 -0400

    infinite questions

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
+  <Head>
+    <Title>Thought Guesser</Title>
+  </Head>
   <div id="app">
     <div>
       <div class="h1">
@@ -8,16 +11,25 @@
       </div>
     </div>
     <div>
-      <div>
-        Is it cats?
+      <div v-if="loading" class="loading">
+        Loading
       </div>
-      <div>
+      <div v-else-if="error" class="error">
+        <div>
+          {{ error }}
+        </div>
       </div>
+      <div v-else class="question">
+        <div>
+          {{ question }}
+        </div>
+      </div>
+      <div></div>
     </div>
     <div>
       <div class="buttons">
-        <button @click="answerYes">Yes</button>
-        <button @click="answerNo">No</button>
+        <button @click="answerYes" type="button">Yes</button>
+        <button @click="answerNo" type="button">No</button>
       </div>
       <div>
       </div>
@@ -32,82 +44,106 @@
 </template>
 
 <script setup>
+import { initializeApp } from 'firebase/app';
+import { getFunctions, connectFunctionsEmulator, httpsCallable } from 'firebase/functions';
+// Initialize Firebase and Firebase Functions
+// Import the functions you need from the SDKs you need
+import { getAnalytics, logEvent, setUserProperties } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfz08B4YL28zJ_Cn2htXyp9evvgv72WcY",
+  authDomain: "thought-guesser.firebaseapp.com",
+  projectId: "thought-guesser",
+  storageBucket: "thought-guesser.appspot.com",
+  messagingSenderId: "322998052824",
+  appId: "1:322998052824:web:d99925b5c935bdba3a74ad",
+  measurementId: "G-7LKX0FK11J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Functions
+const functions = getFunctions(app);
+
+// If development, connect to emulator
+if (import.meta.env.DEV) {
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
+
+const questionId = ref('lTg7v2eujwqh94To36L9');
+const question = ref('Is it an object you can hold in your hand?');
+const sessionId = ref('');
+const browserId = ref('')
+const loading = ref(false);
+const error = ref(null);
+
+// On mount
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  const analytics = getAnalytics(app);
+
+  logEvent(analytics, 'page_view');
+});
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  }
+})
+
+const answerQuestion = async (answer) => {
+  loading.value = true;
+
+  // Call the "answerQuestion" function from Firebase Functions
+  const answerQuestion = httpsCallable(functions, 'answerQuestion');
+
+  if (!browserId.value) {
+    browserId.value = localStorage.getItem('browserId');
+  }
+
+  const analytics = getAnalytics();
+
+  logEvent(analytics, 'answer_question', {
+    question_id: questionId.value,
+    answer: answer ? 'yes' : 'no',
+    question: question.value,
+  });
+
+
+  await answerQuestion({
+    answer: answer,
+    questionId: questionId.value,
+    sessionId: sessionId.value,
+    browserId: browserId.value
+  }).then((result) => {
+    question.value = result.data.nextQuestion;
+    questionId.value = result.data.questionId;
+    sessionId.value = result.data.sessionId;
+    browserId.value = result.data.browserId;
+
+    error.value = null;
+    localStorage.setItem('browserId', browserId.value);
+    setUserProperties(analytics, { browser_id: browserId.value });
+  }).catch((err) => {
+    error.value = "Unexpected error";
+  });
+
+  loading.value = false;
+}
+
+const answerYes = () => {
+  answerQuestion(true)
+}
+
+const answerNo = () => {
+  answerQuestion(false)
+}
+
 </script>
 
 <style>
-  body {
-    color: white;
-    background-color: #1d1d1d;
-    font-family: 'Roboto', sans-serif;
-    margin: 0;
-  }
-
-  .buttons {
-    display: flex;
-    justify-content: flex-end;
-    width: 100px;
-  }
-
-  .h1 {
-    font-size: 1.8rem;
-    font-weight: 700;
-    line-height: 1.5;
-  }
-
-  #app {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    font-size: 1.2rem;
-  }
-
-  #app > div {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    margin-bottom: 1.2rem;
-  }
-
-  #app > div > div {
-    width: 50%;
-  }
-
-  #app > div > div:first-child {
-    text-align: right;
-    padding-right: 1rem;
-  }
-
-  #app > div > div:last-child {
-    text-align: left;
-    padding-left: 1rem;
-  }
-
-  .logo {
-    background-image: url(./assets/logo.png);
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: start;
-    height: 100px;
-  }
-
-  button {
-    background-color: #1d1d1d;
-    border: none;
-    border-radius: 5px;
-    color: #fff;
-    cursor: pointer;
-    font-size: 1rem;
-    font-weight: 700;
-    padding: 10px 20px;
-    text-decoration: underline;
-  }
-
-  button:first-child {
-    margin-right: 1rem;
-  }
-
   /* http://meyerweb.com/eric/tools/css/reset/ */
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -135,7 +171,7 @@
     display: block;
   }
   body {
-    line-height: 1;
+    margin-left: 30px;
   }
   ol, ul {
     list-style: none;
@@ -152,4 +188,101 @@
     border-collapse: collapse;
     border-spacing: 0;
   }
+
+  /* Custom styles */
+  body {
+    color: white;
+    background-color: #1d1d1d;
+    margin: 0;
+    /* Space Mono */
+    font-family: 'Space Mono', monospace !important;
+  }
+
+  @font-face {
+    font-family: 'Space Mono';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/spacemono/v12/i7dPIFZifjKcF5UAWdDRYEF8RQ.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: flex-end;
+    width: 100px;
+  }
+
+  .h1 {
+    font-size: calc(1.2rem + 1vw);
+    font-weight: 700;
+    line-height: 1.5;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    font-size: calc(1rem + 0.5vw);
+    margin-left: 30px;
+  }
+
+  #app > div {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin-bottom: 1.2rem;
+  }
+
+  #app > div > div:first-child {
+    text-align: right;
+    padding-right: 1rem;
+    width: 50%;
+  }
+
+  #app > div > div:last-child {
+    text-align: left;
+    padding-left: 1rem;
+    width: 50%;
+  }
+
+  .question,.loading,.error {
+    min-height: 80px;
+    max-height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    overflow-y: auto;
+    white-space: pre-wrap;
+    min-width: 140px;
+  }
+
+  .logo {
+    background-image: url(./assets/logo.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: start;
+    height: calc(100px + 2.5vw);
+  }
+
+  button {
+    background-color: #1d1d1d;
+    border: none;
+    border-radius: 5px;
+    color: #fff;
+    cursor: pointer;
+    font-size: calc(1rem + 0.5vw);
+    font-weight: 700;
+    padding: 10px 0 10px 20px;
+    text-decoration: underline;
+    text-align: right;
+  }
+
+  button:first-child {
+    margin-right: 1rem;
+  }
+
 </style>
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,91 @@
+const { FieldValue } = require('firebase-admin/firestore');
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Initialize GPT-3
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+    apiKey: functions.config().openai.key
+});
+
+const openai = new OpenAIApi(config);
+
+// Initialize Firestore
+const db = admin.firestore();
+
+// A JSON endpoint that receives a session id, browser id, and answer
+exports.answerQuestion = functions.https.onCall(async (data, context) => {
+  let sessionId = data.sessionId;
+  let browserId = data.browserId;
+  const questionId = data.questionId;
+  const answer = data.answer;
+
+  if (!browserId) {
+    // Add new browser to Firestore
+    const browserRef = await db.collection("browsers").add({})
+    browserId = browserRef.id;
+  }
+
+  if (!sessionId) {
+    // Add new session to Firestore
+    const sessionRef = await db.collection("browsers").doc(browserId).collection("sessions").add({
+      answers: [],
+    })
+    sessionId = sessionRef.id;
+  }
+
+  // Log question id
+  functions.logger.log("Question ID: ", questionId);
+  const questionRef = db.collection("questions").doc(questionId);
+  const question = (await questionRef.get()).data().content;
+
+  const sessionRef = db
+    .collection("browsers").doc(browserId)
+    .collection("sessions").doc(sessionId);
+
+
+  const sessionAnswers = (await sessionRef.get()).data().answers;
+
+  sessionAnswers.push(
+    { browserId, answer, questionId, question }
+  )
+
+  sessionRef.update({
+    answers: sessionAnswers,
+  });
+
+  
+  // get all session answers
+  let prompt = "Assistant is playing 20 questions against User. 20 questions is a game where you try to guess what the user is thinking, using previous answers to make increasingly specific guesses.\n\n"
+
+  sessionAnswers.forEach(answer => {
+    prompt += `Assistant: ${answer.question}\n`;
+    prompt += `User: ${answer.answer ? 'yes' : 'no'}\n`;
+  })
+
+  const questionPrefix = "Is it"
+  prompt += `Assistant: ${questionPrefix}`;
+
+  const response = await openai.createCompletion({
+    prompt,
+    model: "text-davinci-003",
+    max_tokens: 100,
+    temperature: 0,
+    stop: ["User:"]
+  });
+
+  const message = response.data.choices[0].text.trim();
+  console.log(message)
+  let nextQuestion = `${questionPrefix} ${message}`
+  
+  // Add question to question collection.
+  const questionDoc = await db.collection("questions").add({
+    content: nextQuestion,
+    createdAt: FieldValue.serverTimestamp(),
+  });
+
+
+  return { sessionId, browserId, nextQuestion: nextQuestion, questionId: questionDoc.id };
+});
